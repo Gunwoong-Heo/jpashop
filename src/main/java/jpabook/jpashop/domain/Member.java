e9670c05f411d7873894be2faccd5287d8770c7d
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-28 17:04:21 +0900

    entity mapping 실습

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,20 +1,21 @@
 package jpabook.jpashop.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
 
     @Id
     @GeneratedValue // strategy 생략하면 AUTO
+    @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
+
     private String city;
+
     private String street;
+
     private String zip;
 
     public Long getId() {
