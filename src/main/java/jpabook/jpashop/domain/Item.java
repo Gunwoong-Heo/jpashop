5ddbd0dc236a3beb2d38d554fe8c6ab30b374a0a
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-31 05:09:52 +0900

    상속관계 매핑 + @mappedSuperclass 적용

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Item extends BaseEntity {
 
     @Id
     @GeneratedValue
